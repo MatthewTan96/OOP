12dfd33bf8170b60bf3404c535b5039eec8ba1b5
MatthewTan96 <54927234+MatthewTan96@users.noreply.github.com>
2020-10-09 19:25:31 +0800

    Updated Webcontent front end

--- Server/controller/src/main/java/com/IS442/teamsixtester/services/AccountService.java
+++ Server/controller/src/main/java/com/IS442/teamsixtester/services/AccountService.java
@@ -3,8 +3,15 @@ package com.IS442.teamsixtester.services;
 //import com.IS442.teamsixtester.dao.Vessel.VesselDAO;
 import com.IS442.teamsixtester.model.Account.Account;
 import com.IS442.teamsixtester.dao.Vessel.AccountRepository;
+import com.IS442.teamsixtester.model.Vessel.Vessel;
+import com.IS442.teamsixtester.model.Vessel.VesselDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class AccountService {
@@ -19,7 +26,19 @@ public class AccountService {
         accountRepository.delete(account);
     }
 
-    public Account getAccountByEmail(String email){
+    public Account getAccountByEmail(String email) {
         return accountRepository.findTopByEmail(email);
+    }
+
+    public Account updateAccount(Account existingAccount, Account toChangeAccount) {
+        existingAccount.setPassword(toChangeAccount.getPassword());
+        existingAccount.setVerified(toChangeAccount.getVerified());
+        return accountRepository.save(existingAccount);
+    }
+
+    public List<Account> accountGetAll() {
+        List<Account> accounts = new ArrayList<>();
+        accountRepository.findAll().forEach((accounts::add));
+        return accounts;
     }
 }
