4542ba984a9bf0b25abeab2775b94c553d6d3dfe
MatthewTan96 <54927234+MatthewTan96@users.noreply.github.com>
2020-10-09 22:11:12 +0800

    Web content update

--- Server/controller/src/main/java/com/IS442/teamsixtester/controllers/AccountController.java
+++ Server/controller/src/main/java/com/IS442/teamsixtester/controllers/AccountController.java
@@ -6,10 +6,7 @@ import com.IS442.teamsixtester.services.AccountService;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import com.IS442.teamsixtester.model.Account.Account;
 
 import javax.persistence.PreUpdate;
