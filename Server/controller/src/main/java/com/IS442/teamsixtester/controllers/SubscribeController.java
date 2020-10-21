f4fba610c3c6d0ebdb1d65c2c3914ddb2c0ec956
VictorWeiLong <53215558+VictorWeiLong@users.noreply.github.com>
2020-10-21 15:27:34 +0800

    Updates to main and adding favourites
    Updates to main and adding favourites

--- Server/controller/src/main/java/com/IS442/teamsixtester/controllers/SubscribeController.java
+++ Server/controller/src/main/java/com/IS442/teamsixtester/controllers/SubscribeController.java
@@ -8,10 +8,12 @@ import com.IS442.teamsixtester.services.VesselService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+@CrossOrigin(origins="*", allowedHeaders = "*")
 @Controller
 public class SubscribeController {
     @Autowired
